ed8b4de5e2ce533ff7d69d5d57fea5ec851ef646
ramadhan-eko <ramadhaneko027@gmail.com>
2021-04-04 12:35:23 +0700

    DOM selection method

---
+++ script.js
@@ -0,0 +1,20 @@
+// DOM selection
+// document.getElementById() -> element 
+const judul = document.getElementById('judul');
+judul.style.color = 'red';
+judul.style.backgroundColor = '#ccc';
+
+// document.getElementsByTagName -> HTMLCollection
+const p = document.getElementsByTagName('p');
+
+for(let i = 0; i<p.length; i++) {
+	p[i].style.backgroundColor = 'lightblue';
+}
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
+
+// document.getElementByClassName -> HTMLCollection
+const p1 = document.getElementsByClassName('p1')[0];
+p1.innerHTML = 'Ini diubah dari javascript';
+
